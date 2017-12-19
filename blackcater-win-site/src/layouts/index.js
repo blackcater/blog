a9343541b7cacfa1f02bdabce2f774d2ef3f91eb
blackcater <blackcater2015@gmail.com>
2017-12-19 23:17:24 +0800

    Add style for home page

--- blackcater-win-site/src/layouts/index.js
+++ blackcater-win-site/src/layouts/index.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react'
+import { Layout, Icon } from 'antd'
+import Link from 'gatsby-link'
+
+import styles from './index.module.less'
+
+const { Header, Content, Footer } = Layout
 
 export default class MainLayout extends Component {
   constructor(props) {
@@ -11,9 +17,40 @@ export default class MainLayout extends Component {
     const { children } = this.props
 
     return (
-      <div>
-        {children()}
-      </div>
+      <Layout className={styles.main}>
+        <Header className={styles.header}>
+          <div className={styles.title}>
+            <h1>blackcater.</h1>
+          </div>
+          <div className={styles.menu}>
+            <ul className={styles.links}>
+              <li className={styles.link}><Link to="/">Home</Link></li>
+              <li className={styles.link}><Link to="/">Resume</Link></li>
+              <li className={styles.link}><Link to="/">About</Link></li>
+              <li className={styles.link}><Link to="/">Tags</Link></li>
+              <li className={styles.link}><Link to="/">Archive</Link></li>
+              <li className={styles.link}><Link to="/">Projects</Link></li>
+            </ul>
+          </div>
+        </Header>
+        <Content className={styles.content}>
+          {children()}
+        </Content>
+        <Footer className={styles.footer}>
+          <div className={styles['bg-text']}>contact</div>
+          <div className={styles['footer-content']}>
+            <div className={styles['footer-slogan']}>一心所向，必有回响</div>
+            <div className={styles['footer-email']}>blackcater2015@gmail.com</div>
+            <div className={styles['footer-social']}>
+              <a><Icon className={styles['footer-social--icon']} type="twitter" /></a>
+              <a><Icon className={styles['footer-social--icon']} type="facebook" /></a>
+              <a><Icon className={styles['footer-social--icon']} type="google" /></a>
+              <a><Icon className={styles['footer-social--icon']} type="github" /></a>
+              <a><Icon className={styles['footer-social--icon']} type="weibo" /></a>
+            </div>
+          </div>
+        </Footer>
+      </Layout>
     )
   }
 }
--- blackcater-win-site/theme.js
+++ blackcater-win-site/theme.js
@@ -1,3 +1,18 @@
 module.exports = {
-  "primary-color": "#ec5455",
+  "primary-color": "#fb245f",
+
+  "body-background": "#151616",
+  "component-background": "#151616",
+
+  "heading-color": "#ffffff",
+  "text-color": "#aeaeae",
+  "text-color-secondary": "#d0cfd0",
+
+  "border-radius-base": "0px",
+  "border-radius-sm": "0px",
+
+  "layout-body-background": "#151616",
+  "layout-header-background": "#151616",
+  "layout-footer-background": "#151616",
+  "layout-header-height": "150px",
 }
