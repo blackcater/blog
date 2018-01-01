4a6dac28570d5d4f011205e434d93abac09d939f
blackcater <blackcater2015@gmail.com>
2018-01-01 18:53:25 +0800

    Moving ./src/posts to ./posts

--- blackcater-win-site/gatsby-config.js
+++ blackcater-win-site/gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'posts',
-        path: `${__dirname}/src/pages/`,
+        path: `${__dirname}/posts/`,
       },
     },
     'gatsby-transformer-remark',
