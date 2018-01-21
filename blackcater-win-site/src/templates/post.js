b1626698eb9ca0cdb60cd78799e63dc0df5e6e7e
blackcater <blackcater2015@gmail.com>
2018-01-21 22:17:08 +0800

    Add left heading section

--- blackcater-win-site/src/templates/post.js
+++ blackcater-win-site/src/templates/post.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Modal from 'react-modal'
 import { Icon } from 'components'
+import cx from 'classnames'
+import { events, query as domQuery } from 'dom-helpers'
 import Gitment from 'gitment'
 
 import 'styles/gitment.css'
@@ -13,6 +15,8 @@ export default class PostTemplate extends Component {
     this.state = {
       rewardModalOpen: false,
       rewardImageSrc: '',
+      collapse: false,
+      showCollapse: false,
     }
   }
 
@@ -33,6 +37,35 @@ export default class PostTemplate extends Component {
     setTitle(post.frontmatter.title)
 
     gitment.render('comments')
+
+    // 监听滚动事件
+    events.on(window.document, 'scroll', this.handleScroll)
+  }
+
+  componentWillUnmount() {
+    events.off(window.document, 'scroll', this.handleScroll)
+  }
+
+  // 滚动事件
+  handleScroll = e => {
+    const scrollTop = domQuery.scrollTop(e.target)
+    const height = window.innerHeight
+    const { showCollapse } = this.state
+
+    if (scrollTop > height && !showCollapse) {
+      this.setState({ showCollapse: true })
+    }
+
+    if (scrollTop <= height && showCollapse) {
+      this.setState({ showCollapse: false })
+    }
+  }
+
+  // 切换侧边栏
+  handleToggleCollapse = () => {
+    this.setState({
+      collapse: !this.state.collapse,
+    })
   }
 
   // 标签点击
@@ -79,126 +112,152 @@ export default class PostTemplate extends Component {
   }
 
   render() {
-    const { rewardModalOpen, rewardImageSrc } = this.state
+    const {
+      rewardModalOpen,
+      rewardImageSrc,
+      collapse,
+      showCollapse,
+    } = this.state
     const { markdownRemark: post } = this.props.data
     const { prevPost, nextPost, category, tags = [] } = this.props.pathContext
     const prev = prevPost || post
     const next = nextPost || post
 
+    console.dir(post)
+
     return (
-      <div className="template-post">
-        <div className="title">
-          {post.frontmatter.title}{' '}
-          <span
-            className="category"
-            onClick={() => this.handleCategoryClick(category.slug)}
-          >
-            {category.name}
-          </span>
-        </div>
-        <div className="time">{post.frontmatter.edate}</div>
-        <div className="info">
-          <div className="time-to-read">{post.timeToRead}</div>
-          <div className="words-count">{post.wordCount.words}</div>
-        </div>
-        <div className="content">
-          <div
-            className="typograph"
-            dangerouslySetInnerHTML={{ __html: post.html }}
-          />
-        </div>
-        <div className="tags">
-          {tags.map((tag, index) => (
-            <div
-              key={`${index}`}
-              className="tag"
-              onClick={() => this.handleTagClick(tag.slug)}
+      <div className={cx({ 'template-post': true, collapse })}>
+        <div className="post">
+          <div className="title">
+            {post.frontmatter.title}{' '}
+            <span
+              className="category"
+              onClick={() => this.handleCategoryClick(category.slug)}
             >
-              {tag.name}
-            </div>
-          ))}
-        </div>
-        <div className="reward">
-          <img
-            className="reward-image"
-            src={require('images/rewards/img.png')}
-            alt="创作不易，如果你觉得文章不错，来点打赏吧！"
-          />
-          <div className="pay-list">
-            <div
-              className="pay pay-zhifubao"
-              onClick={() => this.handleRewardClick('zhifubao')}
-            >
-              <img src={require('images/rewards/zhifubao.png')} alt="支付宝" />
-              支付宝
-            </div>
-            <div
-              className="pay pay-weixin"
-              onClick={() => this.handleRewardClick('weixin')}
-            >
-              <img src={require('images/rewards/weixin.png')} alt="支付宝" />
-              微信
-            </div>
+              {category.name}
+            </span>
           </div>
-        </div>
-        <div className="navigator-list">
-          <div
-            className="navigator-item navigator-item--prev"
-            onClick={() => this.handlePostClick(prev)}
-          >
+          <div className="time">{post.frontmatter.edate}</div>
+          <div className="info">
+            <div className="time-to-read">{post.timeToRead}</div>
+            <div className="words-count">{post.wordCount.words}</div>
+          </div>
+          <div className="content">
             <div
-              className="cover"
-              style={{ backgroundImage: `url("${prev.frontmatter.cover}")` }}
+              className="typograph"
+              dangerouslySetInnerHTML={{ __html: post.html }}
             />
-            <div className="content">
-              {prevPost ? <Icon type="arrow-left" /> : <Icon type="arrow-up" />}
-              <div className="tip">PREV POST</div>
-              <div className="title">{prev.frontmatter.title}</div>
-            </div>
           </div>
-          <div
-            className="navigator-item navigator-item--next"
-            onClick={() => this.handlePostClick(next)}
-          >
-            <div
-              className="cover"
-              style={{ backgroundImage: `url("${next.frontmatter.cover}")` }}
-            />
-            <div className="content">
-              {nextPost ? (
-                <Icon type="arrow-right" />
-              ) : (
-                <Icon type="arrow-up" />
-              )}
-              <div className="tip">NEXT POST</div>
-              <div className="title">{next.frontmatter.title}</div>
-            </div>
+          <div className="tags">
+            {tags.map((tag, index) => (
+              <div
+                key={`${index}`}
+                className="tag"
+                onClick={() => this.handleTagClick(tag.slug)}
+              >
+                {tag.name}
+              </div>
+            ))}
           </div>
-        </div>
-        <div id="comments" />
-        <Modal
-          isOpen={rewardModalOpen}
-          style={{
-            content: {
-              marginLeft: '-200px',
-              top: '100px',
-              left: '50%',
-              right: 'auto',
-              bottom: '100px',
-              maxWidth: '400px',
-              background: 'none',
-            },
-          }}
-          onRequestClose={() => this.setState({ rewardModalOpen: false })}
-        >
-          <div className="modal--reward">
+          <div className="reward">
             <img
-              src={rewardImageSrc}
-              alt="扫一扫打赏"
-              onClick={() => this.setState({ rewardModalOpen: false })}
+              className="reward-image"
+              src={require('images/rewards/img.png')}
+              alt="创作不易，如果你觉得文章不错，来点打赏吧！"
             />
+            <div className="pay-list">
+              <div
+                className="pay pay-zhifubao"
+                onClick={() => this.handleRewardClick('zhifubao')}
+              >
+                <img
+                  src={require('images/rewards/zhifubao.png')}
+                  alt="支付宝"
+                />
+                支付宝
+              </div>
+              <div
+                className="pay pay-weixin"
+                onClick={() => this.handleRewardClick('weixin')}
+              >
+                <img src={require('images/rewards/weixin.png')} alt="支付宝" />
+                微信
+              </div>
+            </div>
           </div>
-        </Modal>
+          <div className="navigator-list">
+            <div
+              className="navigator-item navigator-item--prev"
+              onClick={() => this.handlePostClick(prev)}
+            >
+              <div
+                className="cover"
+                style={{ backgroundImage: `url("${prev.frontmatter.cover}")` }}
+              />
+              <div className="content">
+                {prevPost ? (
+                  <Icon type="arrow-left" />
+                ) : (
+                  <Icon type="arrow-up" />
+                )}
+                <div className="tip">PREV POST</div>
+                <div className="title">{prev.frontmatter.title}</div>
+              </div>
+            </div>
+            <div
+              className="navigator-item navigator-item--next"
+              onClick={() => this.handlePostClick(next)}
+            >
+              <div
+                className="cover"
+                style={{ backgroundImage: `url("${next.frontmatter.cover}")` }}
+              />
+              <div className="content">
+                {nextPost ? (
+                  <Icon type="arrow-right" />
+                ) : (
+                  <Icon type="arrow-up" />
+                )}
+                <div className="tip">NEXT POST</div>
+                <div className="title">{next.frontmatter.title}</div>
+              </div>
+            </div>
+          </div>
+          <div id="comments" />
+          <Modal
+            isOpen={rewardModalOpen}
+            style={{
+              content: {
+                marginLeft: '-200px',
+                top: '100px',
+                left: '50%',
+                right: 'auto',
+                bottom: '100px',
+                maxWidth: '400px',
+                background: 'none',
+              },
+            }}
+            onRequestClose={() => this.setState({ rewardModalOpen: false })}
+          >
+            <div className="modal--reward">
+              <img
+                src={rewardImageSrc}
+                alt="扫一扫打赏"
+                onClick={() => this.setState({ rewardModalOpen: false })}
+              />
+            </div>
+          </Modal>
+        </div>
+        <div className="headings" />
+        <div
+          className={cx({ 'collapse-icon': true, show: showCollapse })}
+          onClick={this.handleToggleCollapse}
+        >
+          <Icon
+            type={collapse ? 'arrow-left' : 'arrow-right'}
+            style={{ color: '#fff', fontSize: '24px' }}
+          />
+        </div>
       </div>
     )
   }
