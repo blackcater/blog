4415fd22008721d02624b2ef79a87da78a35f963
blackcater <blackcater2015@gmail.com>
2018-12-04 22:21:03 +0800

    feat: add less support

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
 module.exports = {
+  pathPrefix: '/',
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    // https://www.npmjs.com/package/gatsby-plugin-react-helmet
     'gatsby-plugin-react-helmet',
+
+    // https://www.npmjs.com/package/gatsby-source-filesystem
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -11,8 +15,20 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+
+    // https://www.npmjs.com/package/gatsby-transformer-remark
+    'gatsby-transformer-remark',
+
+    // https://www.npmjs.com/package/gatsby-transformer-sharp
     'gatsby-transformer-sharp',
+    // https://www.npmjs.com/package/gatsby-plugin-sharp
     'gatsby-plugin-sharp',
+
+    // http://lesscss.org/
+    'gatsby-plugin-less',
+    'gatsby-plugin-remove-trailing-slashes',
+
+    // https://www.npmjs.com/package/gatsby-plugin-manifest
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
@@ -22,11 +38,11 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/gatsby-icon.png',
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
+
+    // https://gatsby.app/offline
     // 'gatsby-plugin-offline',
   ],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -3,5 +3,59 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
+const path = require('path');
 
-// You can delete this file if you're not using it
+// custom webpack configuration
+exports.onCreateWebpackConfig = ({ stage, actions }) => {
+  actions.setWebpackConfig({
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+    },
+  });
+};
+
+// create pages
+exports.createPages = ({ page, graphql, actions }) => {
+  const { createPage, deletePage } = actions;
+
+  return new Promise((resolve, reject) => {
+    resolve();
+    // const blogPostTemplate = path.resolve(`src/templates/blog-post.js`)
+    // // Query for markdown nodes to use in creating pages.
+    // resolve(
+    //   graphql(
+    //     `
+    //       {
+    //         allMarkdownRemark(limit: 1000) {
+    //           edges {
+    //             node {
+    //               frontmatter {
+    //                 path
+    //               }
+    //             }
+    //           }
+    //         }
+    //       }
+    //     `
+    //   ).then(result => {
+    //     if (result.errors) {
+    //       reject(result.errors)
+    //     }
+    //
+    //     // Create pages for each markdown file.
+    //     result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    //       const path = node.frontmatter.path
+    //       createPage({
+    //         path,
+    //         component: blogPostTemplate,
+    //         // In your blog post template's graphql query, you can use path
+    //         // as a GraphQL variable to query for data from the markdown file.
+    //         context: {
+    //           path,
+    //         },
+    //       })
+    //     })
+    //   })
+    // )
+  });
+};
