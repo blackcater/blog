ab37946194144ce9b6e27d5cf98d9fdcfa522aec
blackcater <blackcater2015@gmail.com>
2018-12-30 23:00:12 +0800

    feat: finish post-big component

--- src/components/Post/PostBig.jsx
+++ src/components/Post/PostBig.jsx
@@ -1,5 +1,5 @@
 import React, { PureComponent } from 'react';
-import { graphql } from 'gatsby';
+import { Link, graphql } from 'gatsby';
 import Img from 'gatsby-image';
 import PropTypes from 'prop-types';
 import pick from 'utils/pick';
@@ -12,9 +12,38 @@ class PostBig extends PureComponent {
 
     return (
       <div className="post-big">
-        <Img fluid={pick(post, 'frontmatter.cover.childImageSharp.fluid')} />
-        <div className="post-big__title">{pick(post, 'frontmatter.title')}</div>
-        <div className="post-big__excerpt">{pick(post, 'excerpt')}</div>
+        <Link to={pick(post, 'fields.slug')}>
+          <Img fluid={pick(post, 'frontmatter.cover.childImageSharp.fluid')} />
+          <div className="post-big__title">
+            {pick(post, 'frontmatter.title')}
+          </div>
+          <div className="post-big__excerpt">{pick(post, 'excerpt')}</div>
+        </Link>
+        <div className="post-big__footer">
+          <div className="post-big__author">
+            <Link to={`/author/${pick(post, 'frontmatter.author.id')}`}>
+              <img
+                className="post-big__author__avatar"
+                src={pick(
+                  post,
+                  'frontmatter.author.avatar.childImageSharp.resize.src'
+                )}
+                alt={pick(post, 'frontmatter.author.nickname')}
+              />
+            </Link>
+            <div className="post-big__author__content">
+              <div className="post-big__author__nickname">
+                <Link to={`/author/${pick(post, 'frontmatter.author.id')}`}>
+                  {pick(post, 'frontmatter.author.nickname')}
+                </Link>
+              </div>
+              <div className="post-big__author__external">
+                <div className="date">{pick(post, 'frontmatter.date')}</div>
+                <div className="ttr">{pick(post, 'timeToRead')}min read</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -31,7 +60,11 @@ PostBig.defaultProps = {
 export const postBigQuery = graphql`
   fragment PostBig on MarkdownRemark {
     id
+    fields {
+      slug
+    }
     excerpt
+    timeToRead
     frontmatter {
       title
       cover {
@@ -43,7 +76,7 @@ export const postBigQuery = graphql`
           }
         }
       }
-      date: date(formatString: "MMMM DD, YYYY")
+      date: date(formatString: "MMM D")
       author {
         id
         nickname
