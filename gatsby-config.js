3d3682268dae635f04168c6b797723f601bf3b32
blackcater <blackcater2015@gmail.com>
2019-02-12 21:38:42 +0800

    【文档】添加第一段内容

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,6 +50,7 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
+              maxWidth: 1200,
               linkImagesToOriginal: false,
               backgroundColor: 'transparent',
             },
