e5fd9ad90a25c9c9eeaa10583e2484d546a95dd8
blackcater <blackcater2015@gmail.com>
2019-02-28 17:24:07 +0800

    feat: redesign 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
+import { StaticQuery, graphql } from 'gatsby';
 
 import Layout from 'components/Layout';
-import Header from 'components/Header';
 
 import './404.less';
 
-const NotFoundPage = () => (
-  <Layout>
-    <Header title="Not Fond" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-);
+class NotFoundPage extends PureComponent {
+  render() {
+    return (
+      <StaticQuery
+        query={graphql`
+          query NotFoundPageQuery {
+            svg: file(relativePath: { eq: "404.svg" }) {
+              publicURL
+            }
+          }
+        `}
+        render={({ svg }) => (
+          <Layout title="Not Fond" class="notfound-page">
+            <div className="notfound-page__wrap">
+              <img src={svg.publicURL} alt="404" />
+
+              <div className="notfound-page__content">
+                <h1>NOT FOUND</h1>
+                <p>Oops, please see other pages.</p>
+              </div>
+            </div>
+          </Layout>
+        )}
+      />
+    );
+  }
+}
 
 export default NotFoundPage;
