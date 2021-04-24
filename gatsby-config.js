f45772ea668e00f311df8e828109ecbc485454bb
blackcater <blackcater2015@gmail.com>
2021-04-24 21:48:18 +0800

    feat: 修复无法运行问题

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  plugins: ["@unisite/theme-blog"],
+  plugins: [
+    "@unisite/theme-blog",
+    {
+      resolve: "gatsby-plugin-compile-es6-packages",
+      options: { modules: ["@unisite/theme-blog"] },
+    },
+  ],
 };
